12ca82a36bf55f05f4b84084300bd4408f398365
bestwxrld <opiatescircle@mail.ru>
2023-04-07 15:40:30 +0300

    fixed step 7 for cc

--- src/games/gcdgame.js
+++ src/games/gcdgame.js
@@ -6,21 +6,15 @@ const gameRules = 'Find the greatest common divisor of given numbers.';
 const getQuestionAndAnswer = () => {
   const firstNumber = randomizer();
   const secondNumber = randomizer();
-  const question = `${firstNumber} ${secondNumber}`
-  if (firstNumber > secondNumber) {
-    for (let i = secondNumber; i > 0; i-=1) {
-      if (firstNumber % i === 0 && secondNumber % i === 0) {
-        const correctAnswer = String(i);
-        return [question, correctAnswer];
-      };
-    };
-  };
-  for (let j = firstNumber; j > 0; j-=1) {
-    if (secondNumber % j === 0 && firstNumber % j === 0) {
-      const correctAnswer = String(j);
-      return [question, correctAnswer];
-    };
-  };
+  const question = `${firstNumber} ${secondNumber}`;
+  let correctAnswer = '';
+  for (let i = Math.min(firstNumber, secondNumber); i > 0; i -= 1) {
+    if (firstNumber % i === 0 && secondNumber % i === 0) {
+      correctAnswer = String(i);
+      break;
+    }
+  }
+  return [question, correctAnswer];
 };
 
 export default () => {
