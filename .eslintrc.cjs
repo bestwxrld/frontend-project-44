e1cd30f45efff1b00e5c6a50ea1ebb839b9b3a1f
bestwxrld <opiatescircle@mail.ru>
2023-04-07 16:30:11 +0300

    fixing eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,15 @@
-plugins:
-  - jest
 env:
-  node: true
-extends:
-  - 'airbnb-base'
-  - 'plugin:jest/recommended'
+  browser: true
+  es2021: true
+extends: airbnb-base
+overrides: []
 parserOptions:
   ecmaVersion: latest
-rules: 
+  sourceType: module
+rules:
   no-console: 0
   import/extensions: 
     - error
     - ignorePackages
     - js: always
   no-underscore-dangle: [2, { "allow": ["__filename", "__dirname"] }]
-  linebreak-style: ["error", process.env.NODE_ENV === 'prod' ? "unix" : "windows"]
