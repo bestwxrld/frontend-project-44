2ff9731f873ebf647f3a36b4693351cbc40ea7c9
bestwxrld <opiatescircle@mail.ru>
2023-04-07 17:14:06 +0300

    eslint fixes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,16 @@
-env:
-  browser: true
-  es2021: true
-extends: airbnb-base
-overrides: []
-parserOptions:
-  ecmaVersion: latest
-  sourceType: module
-rules:
-  no-console: 0
-  import/extensions: 
-    - error
-    - ignorePackages
-    - js: always
-  no-underscore-dangle: [2, { "allow": ["__filename", "__dirname"] }]
-  /*eslint linebreak-style: ["error", "windows"]*/
-  
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
